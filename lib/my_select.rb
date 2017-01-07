181ed6c5695c0ab6706fd7721715bd97924181a5
jason brown <j.grant.brown@gmail.com>
2017-01-07 17:42:51 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,12 @@
 
-
-def my_select(array)
-new_collect=[]
-  i = 0
-  while i < array.length
-    new_collect << yield(array[i])
-    i = i + 1
-  end
-  new_collect
+def my_select(collection)
+ i = 0
+ new_collect = []
+ while i < collection.length
+	 if yield(collection[i])
+		 new_collect << collection[i]
+	 end
+	 i += 1
+	end
+	new_collect
 end
